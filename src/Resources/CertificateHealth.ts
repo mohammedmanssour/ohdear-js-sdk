e76decac55a86c848e8248b8bbde8aa597f72428
Mohammed Manssour <manssour.mohammed@gmail.com>
2019-02-05 05:08:45 +0200

    add certificateHealth Resource

---
+++ src/Resources/CertificateHealth.ts
@@ -0,0 +1,41 @@
+import ApiResource from './ApiResource';
+
+type CertificateDetails = {
+  issuer: string;
+  valid_from: string;
+  valid_until: string;
+};
+
+type CertificateCheck = {
+  type: string;
+  labe: string;
+  passed: boolean;
+};
+
+export default class CertificateHealth extends ApiResource {
+  /**
+   * The details of the certificate that was found for the site.
+   *
+   * @var CertificateDetails
+   */
+
+  public certificateDetails: CertificateDetails;
+
+  /**
+   * An array of checks that were performed on the certificate.
+   *
+   * @var CertificateCheck[]
+   */
+  public certificateChecks: CertificateCheck[];
+
+  /**
+   * An array with all the issuer names in the chain of the certificate.
+   *
+   * @var string[]
+   */
+  public certificateChainIssuers: string[];
+
+  static newInstance(): CertificateHealth {
+    return new CertificateHealth();
+  }
+}
